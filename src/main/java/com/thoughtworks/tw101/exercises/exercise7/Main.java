884655a27e3532d0a2fb0655ccec0533fab28fd0
Aidan Obley <aidan@obley.net>
2016-01-07 10:29:29 -0500

    Refactor E7 after review

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -8,38 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean done = false;
-        int maximum = 100;
-        int minimum = 1;
-        String input;
-        int guess;
-
-        RandomNumber randomNumber = new RandomNumber(minimum, maximum);
-        Scanner scanner = new Scanner(System.in);
-
-        while (done != true) {
-            // Prompt for input, max/min
-            System.out.printf("Guess a number between %d and %d:%n", minimum, maximum);
-            input = scanner.nextLine();
-            guess = Integer.parseInt(input);
-            if (guess > maximum || guess < minimum) {
-                System.out.printf("Guess not between %d and %d.%n", minimum, maximum);
-                continue;
-            }
-            switch (randomNumber.isSecretNumber(guess)) {
-                case -1:
-                    System.out.println("Your guess is lower than the secret number.");
-                    minimum = guess + 1;
-                    break;
-                case 0:
-                    System.out.println("Correct! You win!");
-                    done = true;
-                    break;
-                case 1:
-                    System.out.println("Your guess is higher than the secret number.");
-                    maximum = guess - 1;
-                    break;
-            }
-        }
+        RandomNumber randomNumber = new RandomNumber(1, 100);
+        randomNumber.playGuessingGame();
     }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/RandomNumber.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/RandomNumber.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.tw101.exercises.exercise7;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by adobley on 12/27/15.
@@ -7,22 +8,69 @@ import java.util.Random;
 public class RandomNumber {
 
     private final int SECRET_NUMBER;
+    private boolean gameWon = false;
+    private int maximum;
+    private int minimum;
 
     public RandomNumber(int minimum, int maximum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
         Random random = new Random();
         SECRET_NUMBER = random.nextInt(maximum - minimum + 1) + minimum;
     }
 
-    // Returns -1 if guess is less than secret number
-    // Returns 0 if number is equal to secret number
-    // Returns 1 if number is greater than secret number
-    public int isSecretNumber(int guess) {
-        if (guess < SECRET_NUMBER) {
-            return -1;
-        } else if (guess == SECRET_NUMBER) {
-            return 0;
-        } else {
-            return 1;
+    public void playGuessingGame() {
+        while (!gameWon) {
+            int guess = getGuess();
+            if (isGuessValid(guess)) {
+                checkGuess(guess);
+            }
         }
+    }
+
+    public int getGuess() {
+        System.out.printf("Guess a number between %d and %d:%n", minimum, maximum);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return Integer.parseInt(input);
+    }
+
+    public boolean isGuessValid(int guess) {
+        if (guess <= maximum && guess >= minimum) {
+            return true;
+        }
+        System.out.printf("Guess not between %d and %d.%n", minimum, maximum);
+        return false;
+    }
+
+
+
+    private void checkGuess(int guess) {
+        switch (Integer.compare(SECRET_NUMBER, guess)) {
+            case 1:
+                lowGuess(guess);
+                break;
+            case 0:
+                winGame();
+                break;
+            case -1:
+                highGuess(guess);
+                break;
+        }
+    }
+
+    private void highGuess(int guess) {
+        System.out.println("Your guess is higher than the secret number.");
+        maximum = guess - 1;
+    }
+
+    private void winGame() {
+        System.out.println("Correct! You win!");
+        gameWon = true;
+    }
+
+    private void lowGuess(int guess) {
+        System.out.println("Your guess is lower than the secret number.");
+        minimum = guess + 1;
     }
 }
